dae5c4032a8805d05d248e85c0cd97209123f175
Jitendra Kumar Sharma <gtendr676@gmail.com>
2025-12-09 19:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (48.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,16 @@
 class Solution {
 
     public int fib(int n) {
+        if(n<=1)return n;
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return fibRec(n,dp);
-
+        // Arrays.fill(dp,-1);
+        // return fibRec(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
      public int fibo(int n) {
         //c++;
