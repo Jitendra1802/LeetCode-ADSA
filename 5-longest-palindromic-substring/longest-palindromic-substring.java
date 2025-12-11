7efd1011d51fd40123c56905a4852a0aba851d34
Jitendra Kumar Sharma <gtendr676@gmail.com>
2025-12-11 23:12:30 +0530

    Time: 232 ms (20.76%) | Memory: 55.9 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,13 +1,17 @@
 class Solution {
+    boolean[][] memo;
     public String longestPalindrome(String s) {
         int n= s.length();
+        
+        memo= new boolean[n][n];
+        boolean[][] visited=new boolean[n][n];
 
         int maxLen=1;
         int sp=0;
 
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(isPalindrome(s, i, j)==true){
+                if(isPalindrome(s, i, j, visited)){
                     if(j-i+1>maxLen){
                         maxLen=j-i+1;
                         sp=i;
@@ -17,14 +21,20 @@ class Solution {
         }
         return s.substring(sp,sp+maxLen);
     }
-    private boolean isPalindrome(String s, int i , int j){
+    private boolean isPalindrome(String s, int i , int j, boolean[][]  visited){
         if(i>=j){
             return true;
         }
+        if(visited[i][j]){
+            return memo[i][j];
+        }
+        visited[i][j]=true;
 
         if(s.charAt(i)!=s.charAt(j)){
+            memo[i][j]=false;
             return false;
         }
-        return isPalindrome(s,i+1,j-1);
+        memo[i][j]=isPalindrome(s,i+1,j-1, visited);
+        return memo[i][j];
     }
 }
