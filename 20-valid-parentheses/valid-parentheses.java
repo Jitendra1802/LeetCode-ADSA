40521171ce031195fc4289feae39b71cb287c82e
Jitendra Kumar Sharma <gtendr676@gmail.com>
2025-12-24 11:19:46 +0530

    Time: 3 ms (84.50%) | Memory: 43.4 MB (20.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
-     while(true){
-        if(s.contains("()")){
-            s=s.replace("()","");
-        }else if(s.contains("{}")){
-            s=s.replace("{}","");
-        }else if(s.contains("[]")){
-            s=s.replace("[]","");
-        }else{
-            return s.isEmpty();
+     Stack<Character> stack=new Stack<Character>();
+     for(char c : s.toCharArray()){
+        if(c=='('){
+            stack.push(')');
+        }else if(c=='{'){
+            stack.push('}');
+        }else if(c=='['){
+            stack.push(']');
+        }else if(stack.isEmpty() || stack.pop()!=c){
+            return false;
         }
-     }   
+     }
+     return stack.isEmpty();
     }
 
 }
