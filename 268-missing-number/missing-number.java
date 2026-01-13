d6d06ee747f55bc0c5beafe32e494f7bfdc2785f
Jitendra Kumar Sharma <gtendr676@gmail.com>
2026-01-13 17:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (24.04%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             n1=n1^nums[i];
         }
-        for(int i=1;i<=nums.length;i++){
+        for(int i=0;i<=nums.length;i++){
             n2=n2^i;
         }
         return n1^n2;
